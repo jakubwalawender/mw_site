f8918927519860733191ff99bc6527637a714cf0
Jakub Walawender <jakub.walawender@rax.com.pl>
2023-11-14 20:06:34 +0100

    Wagtail init

---
+++ mw_site/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ut2(gg@izu$7@sd7&qoqt%=p73n40yky7$ms#1@r)x0c+x&9y$"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
