7b759f17aac9187af759863b2f8dc94bcd8038fa
Jakub Walawender <jakub.walawender@rax.com.pl>
2023-11-24 01:08:08 +0100

    Created navbar

--- mw_site/settings/base.py
+++ mw_site/settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'django_browser_reload',
+    'menus',
 ]
 
 MIDDLEWARE = [
@@ -169,3 +170,4 @@ TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+NPM_BIN_PATH = "C:\\Users\\rax\\AppData\\Roaming\\npm\\npm.cmd"
